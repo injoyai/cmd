b7e490ffbcd4538d0285027b3a542537ca724835
钱纯净 <1113655791@qq.com>
2023-07-28 00:13:26 +0800

    优化

--- cmd_handler_upgrade.go
+++ cmd_handler_upgrade.go
@@ -3,14 +3,14 @@ package main
 import (
 	"github.com/injoyai/cmd/resource"
 	"github.com/injoyai/conv"
+	"github.com/injoyai/goutil/oss"
 	"github.com/injoyai/goutil/oss/shell"
 	"github.com/injoyai/logs"
 	"github.com/spf13/cobra"
 )
 
 func handlerUpgrade(cmd *cobra.Command, args []string, flags *Flags) {
-	resource.MustDownload("upgrade", "./", true)
+	resource.MustDownload("upgrade", oss.ExecDir(), true)
 	filename := conv.GetDefaultString("", args...)
-	_, err := shell.Exec("in_upgrade " + filename)
-	logs.PrintErr(err)
+	logs.PrintErr(shell.Start("in_upgrade " + filename))
 }
