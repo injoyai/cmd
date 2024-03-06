e425449dc7dd0c2c2fcf64e5ce4bb34e75d04c4e
injoyai <1113655791@qq.com>
2024-03-06 10:55:01 +0800

    优化

--- cmd_handler.go
+++ cmd_handler.go
@@ -5,7 +5,10 @@ import (
 	_ "github.com/DrmagicE/gmqtt/persistence"
 	_ "github.com/DrmagicE/gmqtt/topicalias/fifo"
 	"github.com/injoyai/cmd/crud"
+	"github.com/injoyai/cmd/resource"
+	"github.com/injoyai/cmd/tool"
 	"github.com/injoyai/conv"
+	"github.com/injoyai/goutil/g"
 	"github.com/injoyai/goutil/oss"
 	"github.com/injoyai/goutil/oss/shell"
 	"github.com/injoyai/goutil/other/notice/voice"
@@ -55,4 +58,16 @@ func handlerKill(cmd *cobra.Command, args []string, flags *Flags) {
 	resp, err := shell.Exec("taskkill /?")
 	logs.PrintErr(err)
 	fmt.Println(resp)
+}
+
+func handlerUpgrade(cmd *cobra.Command, args []string, flags *Flags) {
+	resource.MustDownload(g.Ctx(), &resource.Config{
+		Resource:     "upgrade",
+		Dir:          oss.ExecDir(),
+		ReDownload:   true,
+		ProxyEnable:  true,
+		ProxyAddress: flags.GetString("proxy"),
+	})
+	filename := conv.GetDefaultString("", args...)
+	logs.PrintErr(tool.ShellStart("in_upgrade " + filename))
 }
