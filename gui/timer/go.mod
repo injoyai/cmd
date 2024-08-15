d17860dabad7f22e2277fdce03f1e74b64c90020
injoyai <1113655791@qq.com>
2024-08-15 10:46:39 +0800

    修改定时任务,能后台运行(托盘)

--- gui/timer/go.mod
+++ gui/timer/go.mod
@@ -3,9 +3,13 @@ module timer
 go 1.18
 
 require (
+	github.com/getlantern/systray v1.2.2
+	github.com/injoyai/base v1.0.10
 	github.com/injoyai/conv v1.1.5
 	github.com/injoyai/goutil v0.0.0-20240605035225-b0fe25b001c1
+	github.com/injoyai/logs v1.0.7
 	github.com/injoyai/lorca v0.0.0-20230816093842-66a8b4247238
+	xorm.io/xorm v1.3.2
 )
 
 require (
@@ -17,9 +21,16 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/eclipse/paho.mqtt.golang v1.4.2 // indirect
 	github.com/fatih/color v1.14.1 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
 	github.com/glebarez/go-sqlite v1.21.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/goburrow/serial v0.1.0 // indirect
 	github.com/goccy/go-json v0.8.1 // indirect
@@ -27,9 +38,7 @@ require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/injoyai/base v1.0.10 // indirect
 	github.com/injoyai/io v0.1.8 // indirect
-	github.com/injoyai/logs v1.0.7 // indirect
 	github.com/jmespath/go-jmespath v0.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -38,6 +47,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/opentracing/opentracing-go v1.2.1-0.20220228012449-10b1cf09e00b // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/robertkrimen/otto v0.2.1 // indirect
@@ -63,5 +73,4 @@ require (
 	modernc.org/sqlite v1.21.1 // indirect
 	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
 	xorm.io/core v0.7.3 // indirect
-	xorm.io/xorm v1.3.2 // indirect
 )
--- gui/timer/main.go
+++ gui/timer/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"github.com/getlantern/systray"
+	"github.com/injoyai/base/safe"
 	"github.com/injoyai/conv"
 	"github.com/injoyai/goutil/database/sqlite"
 	"github.com/injoyai/goutil/net/ip"
@@ -50,11 +52,76 @@ func init() {
 }
 
 func main() {
-	lorca.Run(&lorca.Config{
-		Width:  960,
-		Height: 560,
-		Html:   html,
-	}, func(app lorca.APP) error {
+	ui := &ico{
+		gui: &gui{
+			cfg: &lorca.Config{
+				Width:  960,
+				Height: 560,
+				Html:   html,
+			},
+		},
+		Closer: safe.NewCloser(),
+	}
+	ui.SetCloseFunc(func() error {
+		//退出界面
+		ui.gui.Close()
+		//退出图标
+		systray.Quit()
+		return nil
+	})
+
+	systray.Run(ui.onReady, ui.onExit)
+}
+
+type ico struct {
+	gui *gui
+	*safe.Closer
+}
+
+func (this *ico) onReady() {
+	systray.SetIcon(IcoTimer)
+	systray.SetTooltip("定时器")
+
+	//显示菜单,这个库不能区分左键和右键,固设置了该菜单
+	mShow := systray.AddMenuItem("显示", "显示界面")
+	mShow.SetIcon(IcoMenuTimer)
+	go func() {
+		for {
+			<-mShow.ClickedCh
+			//show会阻塞,多次点击无效
+			this.gui.show()
+		}
+	}()
+
+	//退出菜单
+	mQuit := systray.AddMenuItem("退出", "退出程序")
+	mQuit.SetIcon(IcoMenuQuit)
+	go func() {
+		<-mQuit.ClickedCh
+		this.Close()
+	}()
+
+}
+
+func (this *ico) onExit() {
+	logs.Debug(666)
+}
+
+type gui struct {
+	cfg *lorca.Config
+	app lorca.APP
+}
+
+func (this *gui) Close() error {
+	if this.app != nil {
+		return this.app.Close()
+	}
+	return nil
+}
+
+func (this *gui) show() {
+	lorca.Run(this.cfg, func(app lorca.APP) error {
+		this.app = app
 
 		Script.SetFunc("print", func(args *script.Args) (interface{}, error) {
 			s := fmt.Sprint(args.Interfaces()...)
@@ -62,48 +129,48 @@ func main() {
 			return nil, nil
 		})
 
-		Refresh(app)
+		this.Refresh(app)
 
 		app.Bind("addTimer", func(name, cron, content string, enable bool) {
-			if err := AddTimer(name, cron, content, enable); err != nil {
+			if err := this.AddTimer(name, cron, content, enable); err != nil {
 				app.Eval(fmt.Sprintf(`alert("%s");`, err.Error()))
 				return
 			}
-			Refresh(app)
+			this.Refresh(app)
 		})
 
 		app.Bind("updateTimer", func(id, name, cron, content string) {
-			if err := UpdateTimer(id, name, cron, content); err != nil {
+			if err := this.UpdateTimer(id, name, cron, content); err != nil {
 				app.Eval(fmt.Sprintf(`alert("%s");`, err.Error()))
 				return
 			}
-			Refresh(app)
+			this.Refresh(app)
 		})
 
 		app.Bind("enableTimer", func(id string, enable bool) {
-			defer Refresh(app)
+			defer this.Refresh(app)
 			logs.Debug("enableTimer: ", id, enable)
-			if err := EnableTimer(id, enable); err != nil {
+			if err := this.EnableTimer(id, enable); err != nil {
 				app.Eval(fmt.Sprintf(`alert("%s");`, err.Error()))
 				return
 			}
 		})
 
 		app.Bind("delTimer", func(id string) {
-			if err := DelTimer(id); err != nil {
+			if err := this.DelTimer(id); err != nil {
 				app.Eval(fmt.Sprintf(`alert("%s");`, err.Error()))
 				return
 			}
-			Refresh(app)
+			this.Refresh(app)
 		})
 
-		app.Bind("refresh", func() { Refresh(app) })
+		app.Bind("refresh", func() { this.Refresh(app) })
 
 		return nil
 	})
 }
 
-func Refresh(app lorca.APP) {
+func (this *gui) Refresh(app lorca.APP) {
 	data := []*Timer(nil)
 	if err := DB.Find(&data); err != nil {
 		app.Eval(fmt.Sprintf(`alert("%s");`, err.Error()))
@@ -119,7 +186,7 @@ func Refresh(app lorca.APP) {
 	}
 }
 
-func AddTimer(name, cron, content string, enable bool) error {
+func (this *gui) AddTimer(name, cron, content string, enable bool) error {
 	t := &Timer{
 		Name:    name,
 		Cron:    cron,
@@ -144,7 +211,7 @@ func AddTimer(name, cron, content string, enable bool) error {
 	return nil
 }
 
-func UpdateTimer(id, name, cron, content string) error {
+func (this *gui) UpdateTimer(id, name, cron, content string) error {
 	t := new(Timer)
 	if _, err := DB.ID(id).Get(t); err != nil {
 		return err
@@ -174,7 +241,7 @@ func UpdateTimer(id, name, cron, content string) error {
 	return nil
 }
 
-func EnableTimer(id string, enable bool) error {
+func (this *gui) EnableTimer(id string, enable bool) error {
 	t := new(Timer)
 	if _, err := DB.ID(id).Get(t); err != nil {
 		return err
@@ -201,7 +268,7 @@ func EnableTimer(id string, enable bool) error {
 	})
 }
 
-func DelTimer(id string) error {
+func (this *gui) DelTimer(id string) error {
 	_, err := DB.ID(id).Delete(new(Timer))
 	if err != nil {
 		return err
