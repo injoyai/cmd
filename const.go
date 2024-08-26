7bc08f8bbec52205cbf465884e2fdbe0a6755d28
injoyai <1113655791@qq.com>
2024-08-26 16:03:21 +0800

    v1.3.4: 简单的内置多资源轮训下载(下载失败)

--- const.go
+++ const.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	details = []string{
+		"v1.3.4: 简单的内置多资源轮训下载(下载失败)",
 		"v1.3.3: 增加资源proxy.exe,修改ssh的bug,替换in server forward,增加edge_mini资源,修复mqtt客户端的bug",
 		"v1.3.2: 优化了in text,集读文件/替换/切割/获取/编解码于一身,去除了in read和in json,增加PS5的资源",
 		"v1.3.1: 增加曲线绘制in chart ./xxx.csv,数据量小于3.6万时,可以无损绘制,增加in memo open参数",
